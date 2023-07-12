2808d0b1db2b4218844696593608bbd2ce367480
unknown <reingsroman042@gmail.com>
2023-07-12 17:59:25 +0600

    added new portfolio routes

--- Routes/PortfolioRoute.js
+++ Routes/PortfolioRoute.js
@@ -4,40 +4,72 @@ import Portfolio from "../Models/PortfolioModel.js"
 
 const storage = multer.diskStorage({
 
-    destination : function (req, img, cb) {
-        return cb( null, "./uploads/" )
+    destination: function (req, img, cb) {
+        return cb(null, "./uploads/")
     },
 
-    filename : function (req, img, cb) {
-        return cb( null, `${img.originalname}` )
+    filename: function (req, img, cb) {
+        return cb(null, `${img.originalname}`)
     }
 
 })
 
-const upload = multer ({ storage })
+const upload = multer({ storage })
 
 const PortfolioRoute = express.Router()
 
-PortfolioRoute.post("/createportfolio", upload.single("img"),async (req, res)=>{
+PortfolioRoute.post("/createportfolio", upload.single("img"), async (req, res) => {
 
-    console.log(req.file, req.body)
-
-    try{
+    try {
 
         const { filename } = req.file
         const fileUrl = `http://localhost:5000/uploads/${filename}`
 
         const portfolio = new Portfolio({
             ...req.body,
-            img : fileUrl
+            img: fileUrl
         })
 
         await portfolio.save()
         res.status(200).json({ message: 'Data saved successfully' });
-    }catch{
+    } catch {
         console.error(error);
         res.status(500).json({ message: 'Error saving data' });
     }
+
+})
+
+PortfolioRoute.put("/updateportfolio/:id", upload.single("img"), async (req, res) => {
+
+    const portfolio = await Portfolio.findByIdAndUpdate({ _id: req.params.id })
+
+    if (portfolio && req.file) {
+        const { filename } = req.file
+        const fileUrl = `http://localhost:5000/uploads/${filename}`
+
+        portfolio.name = req.body.name || portfolio.name
+        portfolio.category = req.body.category || portfolio.category
+        portfolio.live = req.body.live || portfolio.live
+        portfolio.des = req.body.des || portfolio.des
+        portfolio.fontCode = req.body.fontCode || portfolio.fontCode
+        portfolio.backCode = req.body.backCode || portfolio.backCode
+        portfolio.img = fileUrl || portfolio.img
+    }
+
+    if (portfolio) {
+
+        portfolio.name = req.body.name || portfolio.name
+        portfolio.category = req.body.category || portfolio.category
+        portfolio.live = req.body.live || portfolio.live
+        portfolio.des = req.body.des || portfolio.des
+        portfolio.fontCode = req.body.fontCode || portfolio.fontCode
+        portfolio.backCode = req.body.backCode || portfolio.backCode
+        portfolio.img = req.file || portfolio.img
+    }
+
+    const updatePortfolio = await portfolio.save()
+
+    res.status(200).json({ message: 'Data update successfully', portfolio : updatePortfolio});
 
 })
 
